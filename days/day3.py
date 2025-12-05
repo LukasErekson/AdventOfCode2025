abe68bc97ba597349a8fe7fb14b1f9ec40f291ac
Lukas <Lukas.Erekson@gmail.com>
2025-12-04 22:25:36 -0700

    Day 3: Finish Part 2

--- days/day3.py
+++ days/day3.py
@@ -7,6 +7,7 @@ def main(input_path: str = "inputs/03/input.txt") -> None:
     battery_banks: list[str] = raw_input.split("\n")
 
     part1(battery_banks)
+    part2(battery_banks)
 
     return
 
@@ -26,7 +27,28 @@ def part1(battery_banks: list[str]) -> None:
 def part2(battery_banks: list[str]) -> None:
     joltages: list[int] = []
 
+    for bank in battery_banks:
+        joltages.append(int(largest_joltage(bank)))
+
     print(f"Part 2: The sum of the joltages is {sum(joltages)}")
+
+
+def largest_joltage(sub_bank: str, joltage: str = "", at_least_remaining: int = 11) -> str:
+    # Base case: All that's left is what can be
+    if len(sub_bank) == at_least_remaining or at_least_remaining == -1:
+        return joltage
+    
+    # Find max digit in sub_bank
+    max_digit: str = max(sub_bank)
+    max_digit_idx: int = sub_bank.index(max_digit)
+    sub_sub_bank: str = sub_bank[max_digit_idx + 1:]
+
+    while len(sub_sub_bank) < at_least_remaining:
+        max_digit = max(sub_bank[:max_digit_idx])
+        max_digit_idx = sub_bank.index(max_digit)
+        sub_sub_bank = sub_bank[max_digit_idx + 1:]
+
+    return largest_joltage(sub_sub_bank, joltage + max_digit, at_least_remaining - 1)
 
 
 if __name__ == "__main__":
