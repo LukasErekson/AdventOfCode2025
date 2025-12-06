fc141646e23610340c5f91b9720a04c88dbb6c9f
Lukas <Lukas.Erekson@gmail.com>
2025-12-05 22:08:41 -0700

    Day 2: Finish part 2

--- days/day2.py
+++ days/day2.py
@@ -1,5 +1,5 @@
 from math import log10, floor, ceil
-from itertools import product 
+from itertools import product
 from typing import Generator
 
 def main(input_path: str = "inputs/02/sample.txt") -> None:
@@ -33,21 +33,19 @@ def invalid_id_generator(r: range) -> Generator[int, None, None]:
                     yield full_id_int
 
 def invalid_id_generator_part2(r: range) -> Generator[int, None, None]:
-    power_of_10_start: int = floor(log10(r.start))
-    power_of_10_stop: int = ceil(log10(r.stop))
+    start_str: str = str(r.start)
+    stop_str: str = str(r.stop)
+    start_digit_length: int = len(start_str)
+    end_digit_length: int = len(stop_str)
 
-    for power in range(power_of_10_start, power_of_10_stop + 1):
-        repeat_length: int = power // 2
+    for length in range(max(start_digit_length, 2), end_digit_length + 1):
         for first_digit in range(1, 10):
-            if power % 2 == 0: 
-                full_id_int = int(str(first_digit)*(repeat_length + 1))
-                if full_id_int in r:
-                    yield full_id_int
-            else:
-                for combo in product(range(0, 10), repeat=repeat_length):
-                    half_id_digits = [str(first_digit)] + [str(d) for d in combo]
-                    half_id_str = "".join(half_id_digits)
-                    full_id_str = half_id_str + half_id_str
+            # Repeats of size 1, 2, 3, 4, ..., length // 2
+            max_repeat_length: int = length // 2
+            for chunk_size in range(1, max_repeat_length + 1):
+                for combo in product(range(0, 10), repeat=chunk_size - 1):
+                    chunk_digits: str = "".join([str(first_digit)] + [str(d) for d in combo])
+                    full_id_str: str = chunk_digits * (length // chunk_size)
                     full_id_int = int(full_id_str)
                     if full_id_int in r:
                         yield full_id_int
@@ -64,49 +62,10 @@ def part1(ranges: list[range]) -> None:
 def part2(ranges: list[range]) -> None:
     invalid_ids: list[int] = []
     for r in ranges:
-        print(f"Processing range {r.start}-{r.stop}: {list(invalid_id_generator_part2(r))}")
-        invalid_ids.extend(list(invalid_id_generator_part2(r)))
+        invalid_ids.extend(set(invalid_id_generator_part2(r)))
 
     print(f"Sum of invalid product Ids: {sum(invalid_ids)}")
     return
-"""
-All inputs between 10 digits.
-0-9: 0 (not even) 10^0
-10-99: 9 (multiples of 11) 10^1
-100-999: 9 (XXX)
-1000-9999: 90 ()"XYXY" 9 ops for X, 10 for Y) 10^3
-10000-99999: 9 (XXXXX) 10^4
-100000-999999: 900 ( "XYZXYZ" 9 ops for X, 10 for Y, 10 for Z) 10^5 
-    (or XYXYXY)
-
-Part 1
-|Power of 10|Count of Even Repeats|Options|
-|-----------|---------------------|-------|
-|0          |0                    |X      |
-|1          |9                    |1      |
-|2          |0                    |X      |
-|3          |90                   |2      |
-|4          |0                    |X      |
-|5          |900                  |3      |
-|6          |0                    |X      |
-|7          |9000                 |4      |
-|8          |0                    |X      |
-|9          |90000                |5      |
-
-Part 2
-|Power of 10|Count of repeated num|Options|
-|-----------|---------------------|-------|
-|0          |0                    |X      |
-|1          |9                    |1      |
-|2          |9                    |1      |
-|3          |90                   |2      |
-|4          |90?                    |X      |
-|5          |900                  |3      |
-|6          |0                    |X      |
-|7          |9000                 |4      |
-|8          |0                    |X      |
-|9          |90000                |5      |
-"""
 
 if __name__ == "__main__":
     main()
